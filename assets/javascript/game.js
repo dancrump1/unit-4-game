853b2b634f96df1de9130cabb2a35a71e41bb264
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-10-13 13:33:20 -0400

    num to guess now > 50

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,38 +4,38 @@ var losses = 0;
 var lossesText = document.getElementById("losses-text");
 var score = 0;
 var totalScoreText = document.getElementById("totalScore-text");
-
-var randNum1 = Math.floor(Math.random() * 12)+1;
-var redGemImage = document.getElementById("redGem");
-var randNum2 = Math.floor(Math.random() * 12)+1;
-var blueGemImage = document.getElementById("blueGem");
-var randNum3 = Math.floor(Math.random() * 12)+1;
-var yellowGemImage = document.getElementById("yellowGem");
-var randNum4 = Math.floor(Math.random() * 12)+1;
-var greenGemImage = document.getElementById("greenGem");
-var randNumStart = Math.floor(Math.random() * 50);
+randNumStart = Math.floor(Math.random() * 50)+50;
 var randNumText = document.getElementById("randNum-text");
 
 
+var redGemImage = document.getElementById("redGem");
+var blueGemImage = document.getElementById("blueGem");
+var yellowGemImage = document.getElementById("yellowGem");
+var greenGemImage = document.getElementById("greenGem");
+
+
 function start() {
-    randNumStart = Math.floor(Math.random() * 50);
     randNumText.textContent = randNumStart;
+
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     score = 0;
     totalScoreText.textContent = score;
-    randNum1 = Math.floor(Math.random() * 12)+1;
+
+    randNum1 = Math.floor(Math.random() * 10)+1;
+    randNum3 = Math.floor(Math.random() * 10)+1;
+    randNum2 = Math.floor(Math.random() * 10)+1;
+    randNum4 = Math.floor(Math.random() * 10)+1;
+
     redGemImage = randNum1;
-    console.log(redGemImage);
-    randNum2 = Math.floor(Math.random() * 12)+1;
     blueGemImage = randNum2;
-    console.log(blueGemImage);
-    randNum3 = Math.floor(Math.random() * 12)+1;
     yellowGemImage = randNum3;
-    console.log(yellowGemImage);
-    randNum4 = Math.floor(Math.random() * 12)+1;
     greenGemImage = randNum4;
+
     console.log(greenGemImage);
+    console.log(redGemImage);
+    console.log(blueGemImage);
+    console.log(yellowGemImage);
 };
 
 function checkScore(){
